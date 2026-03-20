699946482fe5522c6823c6a8e0b7a432c453c8e1
卡蕊琴 <caralynn-233@outlook.com>
2026-03-20 09:45:37 +0800

    为 Android 和 Windows 添加复制回调 URL 的功能，并更新相关标签样式

--- Pages/SettingsPage.xaml.cs
+++ Pages/SettingsPage.xaml.cs
@@ -7,9 +7,24 @@ namespace Shared_Joy.Pages;
 /// </summary>
 public partial class SettingsPage : ContentPage
 {
+    private const string AndroidCallback = "sharedjoy://callback";
+    private const string WindowsCallback = "http://127.0.0.1:5432/callback";
+
     public SettingsPage(SettingsViewModel viewModel)
     {
         InitializeComponent();
         BindingContext = viewModel;
+    }
+
+    private async void OnCopyAndroidCallbackTapped(object? sender, TappedEventArgs e)
+    {
+        await Clipboard.SetTextAsync(AndroidCallback);
+        await DisplayAlert("Copied", "Android callback URL copied.", "OK");
+    }
+
+    private async void OnCopyWindowsCallbackTapped(object? sender, TappedEventArgs e)
+    {
+        await Clipboard.SetTextAsync(WindowsCallback);
+        await DisplayAlert("Copied", "Windows callback URL copied.", "OK");
     }
 }
